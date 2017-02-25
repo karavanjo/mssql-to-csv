5bff81c047c30e9b303384fb67f3840f3238f315
Ivan Kavaliou <jownkiv@gmail.com>
2017-02-25 14:53:01 +0300

    connection string was added

--- MsSqlToCsv/Program.cs
+++ MsSqlToCsv/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +13,20 @@ namespace MsSqlToCsv
     {
         static void Main(string[] args)
         {
+            var connectionString =  ConfigurationManager.ConnectionStrings["mssql"].ConnectionString;
+
+            using (var connection = new SqlConnection(connectionString))
+            {   
+                connection.Open();
+                var tables = connection.GetSchema("Tables");
+
+                var tablesNames = new List<string>();
+
+                foreach (DataRow tableInfo in connection.GetSchema("Tables").Rows)
+                {
+                    tablesNames.Add(tableInfo["TABLE_NAME"].ToString());
+                }
+            }
         }
     }
 }
